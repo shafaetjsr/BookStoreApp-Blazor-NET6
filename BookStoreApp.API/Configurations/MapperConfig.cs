639ef765eec93922231afcbd60d2bec2c6ba081b
Shafaetjsr <Shafaet811@gmail.com>
2023-10-24 01:58:57 +0600

    Completed Book API

--- BookStoreApp.API/Configurations/MapperConfig.cs
+++ BookStoreApp.API/Configurations/MapperConfig.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BookStoreApp.API.Data;
 using BookStoreApp.API.Models.Author;
+using BookStoreApp.API.Models.Book;
 
 namespace BookStoreApp.API.Configurations
 {
@@ -11,6 +12,17 @@ namespace BookStoreApp.API.Configurations
             CreateMap<AuthorCreateDto,Author>().ReverseMap();
             CreateMap<AuthorUpdateDto,Author>().ReverseMap();
             CreateMap<AuthorReadOnlyDto,Author>().ReverseMap();
+
+            CreateMap<BookCreateDto, Book>().ReverseMap();
+            CreateMap<BookUpdateDto, Book>().ReverseMap();
+            CreateMap<Book, BookReadOnlyDto>()
+                .ForMember(x=>x.AuthorName, 
+                a=>a.MapFrom(map=> $"{map.Author.FirstName} {map.Author.LastName}"))
+                .ReverseMap();
+            CreateMap<Book, BookDetailsDto>()
+                .ForMember(x => x.AuthorName,
+                a => a.MapFrom(map => $"{map.Author.FirstName} {map.Author.LastName}"))
+                .ReverseMap();
         }
     }
 }
---
+++ BookStoreApp.API/Controllers/BooksController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookStoreApp.API.Data;
+using AutoMapper;
+using BookStoreApp.API.Models.Book;
+using AutoMapper.QueryableExtensions;
+
+namespace BookStoreApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookStoreDbContext _context;
+        private readonly IMapper _mapper;
+
+        public BooksController(BookStoreDbContext context,IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET: api/Books
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BookReadOnlyDto>>> GetBooks()
+        {
+          if (_context.Books == null)
+          {
+              return NotFound();
+          }
+          var booksDtos = await _context.Books
+                .Include(x=>x.Author)
+                .ProjectTo<BookReadOnlyDto>(_mapper.ConfigurationProvider)
+                .ToListAsync();
+            //var booksDtos= _mapper.Map< IEnumerable<BookReadOnlyDto>>(books);
+            return Ok(booksDtos);
+        }
+
+        // GET: api/Books/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BookDetailsDto>> GetBook(int id)
+        {
+          if (_context.Books == null)
+          {
+              return NotFound();
+          }
+            //var book = await _context.Books.FindAsync(id);
+            var book = await _context.Books
+               .Include(x => x.Author)
+               .ProjectTo<BookDetailsDto>(_mapper.ConfigurationProvider)
+               .FirstOrDefaultAsync(x=>x.Id==id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        // PUT: api/Books/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBook(int id, BookUpdateDto bookDto)
+        {
+            if (id != bookDto.Id)
+            {
+                return BadRequest();
+            }
+            var book = await _context.Books.FindAsync(id);
+            if(book == null)
+            {
+                return BadRequest();
+            }
+            _mapper.Map(bookDto, book);
+            _context.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await BookExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Books
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBook(BookCreateDto bookDto)
+        {
+          if (_context.Books == null)
+          {
+              return Problem("Entity set 'BookStoreDbContext.Books'  is null.");
+          }
+          var book = _mapper.Map<Book>(bookDto);
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
+        }
+
+        // DELETE: api/Books/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            if (_context.Books == null)
+            {
+                return NotFound();
+            }
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private async Task<bool> BookExists(int id)
+        {
+            //return (_context.Books?.Any(e => e.Id == id)).GetValueOrDefault();
+            return await _context.Books.AnyAsync(x=>x.Id==id);
+        }
+    }
+}
